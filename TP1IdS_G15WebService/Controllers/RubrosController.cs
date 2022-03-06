6d28392dc481820456b4f51b51ed6fb318a8a9b6
FacuOrellana <facundo.ore@hotmail.com>
2022-03-06 15:58:10 -0300

    Revert "desarollo de controladores y managers para rubro, talle y color"
    This reverts commit 8d7e3dba1b4eaa8cb99d670596232ca4927cb50d.

--- TP1IdS_G15WebService/Controllers/RubrosController.cs
+++ TP1IdS_G15WebService/Controllers/RubrosController.cs
@@ -9,8 +9,7 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Web.Http.Description;
-using System.Web.WebPages.Html;
-using TP1IdS_G15Application;
+using TP1IdS_G15AccesoADatos;
 using TP1IdS_G15Modelo.Entidades;
 
 namespace TP1IdS_G15WebService.Controllers
@@ -19,20 +18,19 @@ namespace TP1IdS_G15WebService.Controllers
     [RoutePrefix("")]
     public class RubrosController : ApiController
     {
-        
-        private RubrosManager AppLayer = new RubrosManager();
+        private DataContext db = new DataContext();
 
         // GET: api/Rubroes
-        public HttpResponseMessage GetRubros()
+        public IQueryable<Rubro> GetRubros()
         {
-            return Request.CreateResponse(HttpStatusCode.OK, AppLayer.GetRubros());
+            return db.Rubros;
         }
 
         // GET: api/Rubroes/5
         [ResponseType(typeof(Rubro))]
         public IHttpActionResult GetRubro(int id)
         {
-            Rubro rubro = AppLayer.FindRubro(id);
+            Rubro rubro = db.Rubros.Find(id);
             if (rubro == null)
             {
                 return NotFound();
@@ -43,7 +41,7 @@ namespace TP1IdS_G15WebService.Controllers
 
         // PUT: api/Rubroes/5
         [ResponseType(typeof(void))]
-        public IHttpActionResult PutTalle(int id, Rubro rubro)
+        public IHttpActionResult PutRubro(int id, Rubro rubro)
         {
             if (!ModelState.IsValid)
             {
@@ -55,14 +53,15 @@ namespace TP1IdS_G15WebService.Controllers
                 return BadRequest();
             }
 
+            db.Entry(rubro).State = EntityState.Modified;
 
             try
             {
-                AppLayer.SaveRubro(rubro);
+                db.SaveChanges();
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!AppLayer.RubroExists(id))
+                if (!RubroExists(id))
                 {
                     return NotFound();
                 }
@@ -84,22 +83,24 @@ namespace TP1IdS_G15WebService.Controllers
                 return BadRequest(ModelState);
             }
 
-            AppLayer.SaveRubro(rubro);
+            db.Rubros.Add(rubro);
+            db.SaveChanges();
 
             return CreatedAtRoute("DefaultApi", new { id = rubro.Id }, rubro);
         }
 
         // DELETE: api/Rubroes/5
-        [ResponseType(typeof(Talle))]
+        [ResponseType(typeof(Rubro))]
         public IHttpActionResult DeleteRubro(int id)
         {
-            Rubro rubro = AppLayer.FindRubro(id);
+            Rubro rubro = db.Rubros.Find(id);
             if (rubro == null)
             {
                 return NotFound();
             }
 
-            AppLayer.DeleteRubro(id);
+            db.Rubros.Remove(rubro);
+            db.SaveChanges();
 
             return Ok(rubro);
         }
@@ -108,109 +109,14 @@ namespace TP1IdS_G15WebService.Controllers
         {
             if (disposing)
             {
-                AppLayer.Dispose();
+                db.Dispose();
             }
             base.Dispose(disposing);
         }
 
+        private bool RubroExists(int id)
+        {
+            return db.Rubros.Count(e => e.Id == id) > 0;
+        }
     }
-
-    //// GET: api/Rubroes
-    //public IQueryable<Rubro> GetRubros()
-    //{
-    //    return db.Rubros;
-    //}
-
-    //// GET: api/Rubroes/5
-    //[ResponseType(typeof(Rubro))]
-    //public IHttpActionResult GetRubro(int id)
-    //{
-    //    Rubro rubro = db.Rubros.Find(id);
-    //    if (rubro == null)
-    //    {
-    //        return NotFound();
-    //    }
-
-    //    return Ok(rubro);
-    //}
-
-    //// PUT: api/Rubroes/5
-    //[ResponseType(typeof(void))]
-    //public IHttpActionResult PutRubro(int id, Rubro rubro)
-    //{
-    //    if (!ModelState.IsValid)
-    //    {
-    //        return BadRequest(ModelState);
-    //    }
-
-    //    if (id != rubro.Id)
-    //    {
-    //        return BadRequest();
-    //    }
-
-    //    db.Entry(rubro).State = EntityState.Modified;
-
-    //    try
-    //    {
-    //        db.SaveChanges();
-    //    }
-    //    catch (DbUpdateConcurrencyException)
-    //    {
-    //        if (!RubroExists(id))
-    //        {
-    //            return NotFound();
-    //        }
-    //        else
-    //        {
-    //            throw;
-    //        }
-    //    }
-
-    //    return StatusCode(HttpStatusCode.NoContent);
-    //}
-
-    //// POST: api/Rubroes
-    //[ResponseType(typeof(Rubro))]
-    //public IHttpActionResult PostRubro(Rubro rubro)
-    //{
-    //    if (!ModelState.IsValid)
-    //    {
-    //        return BadRequest(ModelState);
-    //    }
-
-    //    db.Rubros.Add(rubro);
-    //    db.SaveChanges();
-
-    //    return CreatedAtRoute("DefaultApi", new { id = rubro.Id }, rubro);
-    //}
-
-    //// DELETE: api/Rubroes/5
-    //[ResponseType(typeof(Rubro))]
-    //public IHttpActionResult DeleteRubro(int id)
-    //{
-    //    Rubro rubro = db.Rubros.Find(id);
-    //    if (rubro == null)
-    //    {
-    //        return NotFound();
-    //    }
-
-    //    db.Rubros.Remove(rubro);
-    //    db.SaveChanges();
-
-    //    return Ok(rubro);
-    //}
-
-    //protected override void Dispose(bool disposing)
-    //{
-    //    if (disposing)
-    //    {
-    //        db.Dispose();
-    //    }
-    //    base.Dispose(disposing);
-    //}
-
-    //private bool RubroExists(int id)
-    //{
-    //    return db.Rubros.Count(e => e.Id == id) > 0;
-    //}
 }
